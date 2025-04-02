4a3137eab489701eb63759191d130c590910e287
tingedudu8 <emberdudu8@gmail.com>
2025-04-01 19:37:36 -0700

    feat: add tabs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
 import './App.css';
+import * as React from 'react';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
+
+function TabContent(props) {
+  const { children, value, index } = props;
+
+  return (
+    <div
+      hidden={value !== index}
+    >
+      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+    </div>
+  );
+}
 
 function App() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
     <div className="App">
-        <p>
-          Ember Start Version1
-        </p>
+        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+          <Tab label="Item One"/>
+          <Tab label="Item Two" />
+          <Tab label="Item Three"/>
+        </Tabs>
+
+        <TabContent value={value} index={0}>
+           Item One
+          </TabContent>
+          <TabContent value={value} index={1}>
+            Item Two
+          </TabContent>
+          <TabContent value={value} index={2}>
+            Item Three
+          </TabContent>
     </div>
   );
 }
